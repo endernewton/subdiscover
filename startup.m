3400c9873d0af89db5568eb48c0faf800e3b8efc
Ender Chen <endernewton@gmail.com>
2014-10-06 22:26:31 -0400

    ack

--- startup.m
+++ startup.m
@@ -25,12 +25,12 @@ options.fixedSeed = 1; % if use fixed random seed
 
 %% Below is the options for all paths, for new datasets you will have to set the path yourself
 % data
-options.datafolder = '/home/xinleic/Segmentation/DataR/'; % directory that contains the datasets to run, in the format of PATH/DATASET/CATEGORY/, 
-% e.g. Rubinstein100/Car/ has .jpg images, where Rubinstein100/Car/GroundTruth has .png ground truth.
+options.datafolder = '/home/xinleic/Segmentation/DataR/'; % PATH to the directory that contains all the datasets to run: it will loop over all the datasets and categories under PATH, 
+% e.g. PATH/Rubinstein100/Car/ has .jpg images, where PATH/Rubinstein100/Car/GroundTruth has .png ground truth.
 options.cachefolder = '/home/xinleic/Segmentation/CachesR/'; % cache intermediate files
 options.resultfolder = '/home/xinleic/Segmentation/ResultsR/'; % folder that holds the result
 options.eldafile = [pwd,'/subclass/elda/background.mat']; % background file for ELDA, representing the negative world
-options.generalpriorfile = [pwd,'/prior.mat']; % segmentation prior for objects, used when no segmentation prior is learned for that class
+options.generalpriorfile = [pwd,'/grabcut/prior.mat']; % segmentation prior for objects, used when no segmentation prior is learned for that class
 options.lsvmNegFolders = {'/home/xinleic/Datasets/Google/Images/ScenesSub/'}; % negative image sets used for training detectors
 
 % elda Training/Testing
